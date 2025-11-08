9122225d2c15bb82ecddf7af1c9de9eee1e40e8a
mahmouddoma <devdoma2002@gmail.com>
2025-11-08 09:24:01 +0200

    add assets

--- src/app/Feature/products-component/products-component.component.ts
+++ src/app/Feature/products-component/products-component.component.ts
@@ -20,37 +20,37 @@ export class ProductsComponentComponent {
       name: 'Zy-Grow 20-20-20',
       desc: 'NPK متوازن للنمو الخضري المبكر.',
       tag: 'الأكثر طلبًا',
-      img: '../../../assets/images/product1.jpg',
+      img: 'assets/images/product1.jpg',
     },
     {
       id: 2,
       name: 'Zy-Cal Boron',
       desc: 'كالسيوم + بورون لتقوية الخلايا وتحسين العقد.',
-      img: '../../../assets/images/product2.jpg',
+      img: 'assets/images/product2.jpg',
     },
     {
       id: 3,
       name: 'Zy-Micro Mix',
       desc: 'عناصر صغرى مخلبة لرفع كفاءة التمثيل.',
-      img: '../../../assets/images/product3.jpg',
+      img: 'assets/images/product3.jpg',
     },
     {
       id: 4,
       name: 'Zy-Root Booster',
       desc: 'منشّط جذور لزيادة الامتصاص.',
-      img: '../../../assets/images/product1.jpg',
+      img: 'assets/images/product1.jpg',
     },
     {
       id: 5,
       name: 'Zy-K Potassium',
       desc: 'بوتاسيوم عالي للجودة وتحسين التلوّن.',
-      img: '../../../assets/images/product2.jpg',
+      img: 'assets/images/product2.jpg',
     },
     {
       id: 6,
       name: 'Zy-Amino',
       desc: 'أحماض أمينية لتحمل الإجهاد.',
-      img: '../../../assets/images/product3.jpg',
+      img: 'assets/images/product3.jpg',
     },
   ];
 }
